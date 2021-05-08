440ebc0dccc4d4498d42ca96d61650d212fb1ce0
Mohammad Belal <king.cseiu@gmail.com>
2021-05-08 17:19:23 +0600

    deleted previous all and create new angular project

--- RxJs-Angular/karma.conf.js
+++ RxJs-Angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rxjs-angular'),
+      dir: require('path').join(__dirname, './coverage/RxJs-Angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
